6cb89d498cb38f6fd7ee0548231e6a16c2b427a6
Ezequiel Santos <elequiel@hotmail.com>
2020-10-29 22:21:15 -0300

    criado serializers

--- restapiecs/apitest/views.py
+++ restapiecs/apitest/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from restapiecs.apitest.serializers import UserSerializer, GroupSerializer
 # Create your views here.

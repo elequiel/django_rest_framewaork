f8f03645b9c77e9616dfceb5a1673fb5de9354a7
Ezequiel Santos <elequiel@hotmail.com>
2020-10-29 22:26:28 -0300

    Criado as viewsets

--- restapiecs/apitest/views.py
+++ restapiecs/apitest/views.py
@@ -4,3 +4,14 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from restapiecs.apitest.serializers import UserSerializer, GroupSerializer
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
